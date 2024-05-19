85c2b2d0cf79baadcb840091d87a2ad560a190e1
Francisco Rubio Santiesteban <115087513+antifull@users.noreply.github.com>
2024-05-19 19:37:06 -0300

    primer commit

---
+++ src/main/java/com/antifull/servicioventanilla/ServicioVentanillaApplication.java
@@ -0,0 +1,13 @@
+package com.antifull.servicioventanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioVentanillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioVentanillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antifull/servicioventanilla/ServicioVentanillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antifull.servicioventanilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioVentanillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
